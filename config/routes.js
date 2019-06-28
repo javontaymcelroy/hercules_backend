1f38d75c607df3447c38ac84e1ff70b1cfb7b89c
Javontay McElroy <javontaymcelroy@Javontays-MBP.attlocal.net>
2019-06-28 11:30:36 -0500

    My log in is broketh

--- config/routes.js
+++ config/routes.js
@@ -9,6 +9,8 @@ const Users = require("../auth/authenticate.js");
 
 module.exports = server => {
   server.post("/register", register);
+  server.post("/login", login);
+  server.get("/users", users);
 };
 
 function register(req, res) {
@@ -23,4 +25,60 @@ function register(req, res) {
     .catch(error => {
       res.status(500).json(error);
     });
+}
+
+function login(req, res) {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrpyt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        const id = user.id;
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token,
+          id
+        });
+      } else {
+        res.status(401).json({ message: "username or password is incorrect" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "5d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+function users(req, res) {
+  Users.find()
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => res.send(err));
+}
+
+function only(username) {
+  return function(req, res, next) {
+    if (req.headers.username === username) {
+      next();
+    } else {
+      res
+        .status(403)
+        .json({ message: "Bruh, leave, you are not suppose to be here!" });
+    }
+  };
 }
