8bd0ad2d73515e0054c125b9cdab34422cbe6efc
Javontay McElroy <46494741+javontaymcelroy@users.noreply.github.com>
2019-06-29 00:30:04 -0500

    Update dbConfig.js

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
 const knex = require("knex");
 
-const setupPaginator = require("knex-paginator");
-
 const knexConfig = require("../knexfile");
 const environment = process.env.DB_ENV || "development";
 const db = knex(knexConfig[environment]);
-setupPaginator(db);
+
 module.exports = db;
